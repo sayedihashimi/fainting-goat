c76534414c7a13c197d9ec25f2175784c192a231
brady gaster <brady.gaster@gmail.com>
2012-09-06 22:17:54 -0700

    cleanup push GH-3

--- fainting-goat/ImageHandler.cs
+++ fainting-goat/ImageHandler.cs
@@ -2,13 +2,16 @@
     using fainting.goat.common;
     using Ninject;
     using System;
-    using System.Collections.Generic;
     using System.IO;
-    using System.Linq;
     using System.Threading;
     using System.Web;
 
     public class ImageHandler : IHttpAsyncHandler {
+
+        public ImageHandler() {
+
+        }
+
         public IAsyncResult BeginProcessRequest(HttpContext context, AsyncCallback cb, object extraData) {
             AsyncOperation async = new AsyncOperation(cb, context, extraData);
             async.StartAsyncWork();
